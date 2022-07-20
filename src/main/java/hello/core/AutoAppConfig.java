c341ecd4dbf14da1e609e15bb5bc5dbc26947d16
HaeChan Kim <bluesun147@naver.com>
2022-07-20 09:34:38 +0900

    필터

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        //basePackages = "hello.core.member", // 탐색할 패키지의 시작 위치
         // 뺄 것들 지정. 수동등록이기 때문에. 실무에서는 그냥 다 함. 예제 코드 살리기 위해서.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
---
+++ src/test/java/hello/core/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package hello.core.scan.filter;
+
+@MyIncludeComponent // 컴포넌트 스캔에 포함
+public class BeanA {
+}
